6d7e505cacdb80e07f8e15d2e8694c491cc733b6
Aarya Agrawal <aarya6105@gmail.com>
2025-02-25 19:50:43 +0530

    completed basic cli setup for creating a db

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const {PORT} = require('./config/serverConfig')
 const setupAndStartServer = async () =>{
     //create the express app
     const app = express();
    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+    
     app.listen(PORT , ()=>{
         console.log(`Server is listening on the port ${PORT}`);
    
